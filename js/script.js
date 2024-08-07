5c4c2b34ba68ec963bd7ce5226c317962c2bdeaa
tarikul127 <158174361+tarikul127@users.noreply.github.com>
2024-08-07 21:18:57 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/* ex1*/
+function showText(){
+	document.getElementById('s_text').innerHTML="Hellow SSB";
+}
+
+/* ex2*/
+function showText1(){
+	document.getElementById('s1_text').innerHTML="Tarikul Islam";
+}
+
+/* ex 3*/
+function showText2(){
+	document.getElementById('s2_text').innerHTML="23326687";
+}
+/* ex 4*/
+function showText3(){
+	document.getElementById('s3_text').innerHTML="Bangladesh";
+}
+
+/* ex 5*/
+function showText4(){
+	document.getElementById('s4_text').innerHTML="Wellcome";
+}
+
+/* ex 6*/
+function showText5(){
+	document.getElementById('s5_text').innerHTML="No text hear";
+}
+
+/* ex 7*/
+function showText6(){
+	document.getElementById('s6_text').innerHTML="I love MY country";
+}
+
+/* ex 8*/
+function showText7(){
+	document.getElementById('s7_text').innerHTML="I love MY Mothert";
+}
